e28035a4f564feae37cd6ec8c0d4ab691f7f6cfa
Elianderson Magalhães R <110203451+EliandersonMR@users.noreply.github.com>
2025-06-02 20:39:52 -0300

    incluir todos os parâmetros necessários.

--- modelos/entrega.py
+++ modelos/entrega.py
@@ -1,8 +1,9 @@
 class Entrega:
-    def __init__(self, destino, prazo, carga):
+    def __init__(self, origem, destino, quantidade, caminhao):
+        self.origem = origem
         self.destino = destino
-        self.prazo = prazo
-        self.carga = carga
+        self.quantidade = quantidade
+        self.caminhao = caminhao
 
     def __repr__(self):
         return f"Entrega para {self.destino} - Prazo: {self.prazo} - Carga: {self.carga} unidades"
